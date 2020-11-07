ee24e8e3d0ae3be630ec8b5f625823b58e4bc4b2
Cheng <hzzc1995@gmail.com>
2020-11-07 05:20:18 -0500

    session router

--- routes/session.js
+++ routes/session.js
@@ -1,5 +1,7 @@
 // login, logout, register
 import express from 'express';
+import User from '../models/user';
+import md5 from 'blueimp-md5';
 
 const router = express.Router();
 
@@ -12,8 +14,50 @@ router.get('/register', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    req.body;
-    res.render('index.html');
+
+    const body = req.body;
+
+    User.findOne({
+        $or: [{
+            email: body.email
+        },
+        {
+            nickname: body.nickname
+        }]
+    }, (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                err_code: 500,
+                message: 'Server Error'
+            });
+        }
+
+        // email/nickname exists
+        if (data) {
+            // return JSON response
+            return res.status(200).json({
+                err_code: 1,
+                message: 'email or nickname already exists'
+            });
+        }
+
+        body.password = md5(md5(body.password));
+        new User(body).save((err, user) => {
+            if (err) {
+                return res.status(500).json({
+                    err_code: 500,
+                    message: 'Server Error'
+                });
+            }
+
+            // response can not be string since the ajax request requires a json format response
+            // the provided json method by Express can convert the object to string and send to browser
+            res.status(200).json({
+                err_code: 0,
+                message: 'ok'
+            });
+        });
+    });
 });
 
 router.get('/', (req, res) => {
